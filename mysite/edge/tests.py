9443cbc96a625afffaea05f3f0262546ba9f90df
Ben Amor <b.r.c.amor@gmail.com>
2014-12-22 15:24:28 +0000

    Basic home page view now returns minimal HTML

--- mysite/edge/tests.py
+++ mysite/edge/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from edge.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -10,3 +11,9 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Allosteric site prediction</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- mysite/edge/views.py
+++ mysite/edge/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    pass
+    return HttpResponse('<html><title>Allosteric site prediction</title></html>')
