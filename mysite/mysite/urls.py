9593ea28a54455fd0645b8a0fe09e6804ed15a9b
brcamor <b.r.c.amor@gmail.com>
2015-01-14 15:13:08 +0000

    jsmol in setup pages, chains and residues highlighted on selection

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -16,3 +16,9 @@ urlpatterns = patterns('',
     url(r'^results', 'edge.views.results', name='results'),
     
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
